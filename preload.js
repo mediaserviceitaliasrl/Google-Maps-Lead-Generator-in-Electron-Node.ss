89a7fb23d49994be0aead4547b976c5087e74dd2
Antonio Galluccio Mezio <galluccioma@gmail.com>
2024-11-13 22:55:58 +0100

    push FINE PER OGGI

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+// Esponi in modo sicuro l'API nel processo renderer
 contextBridge.exposeInMainWorld('electron', {
     startScraping: (searchString) => ipcRenderer.invoke('start-scraping', searchString),
-    onStatus: (callback) => ipcRenderer.on('status', callback)
+    onStatus: (callback) => ipcRenderer.on('status', (event, message) => callback(message)), // Passa il callback direttamente
 });
